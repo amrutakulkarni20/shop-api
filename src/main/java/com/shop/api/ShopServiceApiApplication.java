bddfbd7cb10055018fa38a53487167d8ece024d9
Amruta Kulkarni <130090485+amrutakulkarni20@users.noreply.github.com>
2023-07-05 19:33:54 +0200

    Initial commit

---
+++ src/main/java/com/shop/api/ShopServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.shop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopServiceApiApplication.class, args);
+	}
+
+}
