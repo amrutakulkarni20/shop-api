8e15e52c7c2fff40d2fbdcb83a1a8b3f83319dc8
Amruta Kulkarni <amruta.kulkarni.20121991@gmail.com>
2023-07-10 19:26:24 +0200

    added some changes for ShopControllerTest

--- src/test/java/com/shop/api/controller/ShopControllerTest.java
+++ src/test/java/com/shop/api/controller/ShopControllerTest.java
@@ -3,11 +3,11 @@ package com.shop.api.controller;
 import com.shop.api.ShopServiceApiApplication;
 import com.shop.api.model.Category;
 import com.shop.api.model.ShopModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
@@ -15,11 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.web.client.RestTemplate;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -39,92 +36,67 @@ class ShopControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    @BeforeEach
+    public void setup() {
+        createShop();
+    }
+
     @Test
     void createsShopsAndVerifiesStatusCode() {
-
-        ResponseEntity<List<ShopModel>> createShopsApiResponse = testRestTemplate.exchange(createURLForGetRequest("/shops", host, testPort),
-                HttpMethod.POST, null, new ParameterizedTypeReference<>() {
-                });
+        ResponseEntity<List<ShopModel>> createShopsApiResponse = createShop();
         assertNotNull(createShopsApiResponse);
         assertSame(HttpStatus.CREATED, createShopsApiResponse.getStatusCode());
     }
 
+    private ResponseEntity<List<ShopModel>> createShop(){
+        ResponseEntity<List<ShopModel>> shops = testRestTemplate.exchange(createURLForGetRequest("/shops", host, testPort),
+                HttpMethod.POST, null, new ParameterizedTypeReference<>() {});
+        return shops;
+    }
+
     @Test
-    void getAllShopsAndVerifiesResponse() {
-        ResponseEntity<List<ShopModel>> shops = getShop();
+    void getsAllShopsAndVerifiesStatusCode() {
+        ResponseEntity<List<ShopModel>> shops = testRestTemplate.exchange(createURLForGetRequest("/shopList", host, testPort),
+                HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
         assertNotNull(shops);
         assertSame(HttpStatus.OK, shops.getStatusCode());
     }
 
-    private ResponseEntity<List<ShopModel>> getShop() {
-        ResponseEntity<List<ShopModel>> shops = testRestTemplate.exchange(createURLForGetRequest("/shopList", host, testPort),
-                HttpMethod.GET, null, new ParameterizedTypeReference<>() {
-                });
-        return shops;
-    }
-
     @Test
-    void getShopsAndVerifiesShopId() {
-        String id = "548446d66f9c421d61bb825d";
-        ShopModel shop = getShopById(id);
-        assertNotNull(shop);
-        assertEquals(id, shop.getId());
-    }
-
-    private ShopModel getShopById(String id) {
+    void getsShopByIdAndVerifiesShopId() {
+        final String id = "548446d66f9c421d61bb825d";
         ResponseEntity<ShopModel> shop = testRestTemplate.exchange(createURLForGetRequest("/shop/" + id, host, testPort),
-                HttpMethod.GET, null, new ParameterizedTypeReference<>() {
-                });
-        return shop.getBody();
+                HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+        assertNotNull(shop);
+        assertEquals(id, shop.getBody().getId());
     }
 
     @Test
-    void updateShopAndVerifiesResponse() {
-        ResponseEntity<ShopModel> shopModel = createShopRequest();
-        ResponseEntity<ShopModel> shop = updateShop(shopModel);
-        assertNotNull(shop);
-        assertSame(HttpStatus.CREATED, shop.getStatusCode());
-    }
-
-    private ResponseEntity<ShopModel> updateShop(ResponseEntity<ShopModel> shopModel) {
-        ResponseEntity<List<ShopModel>> createShopsApiResponse = testRestTemplate.exchange(createURLForGetRequest("/shops", host, testPort),
-                HttpMethod.POST, null, new ParameterizedTypeReference<>() {
-                });
-
+    void updateShopAndVerifiesStatusCode() {
+        ResponseEntity<ShopModel> updateShopRequest = createShopRequest();
         ResponseEntity<ShopModel> updateShopsApiResponse = testRestTemplate.exchange(createURLForGetRequest("/shop", host, testPort),
-                HttpMethod.PUT, shopModel, new ParameterizedTypeReference<>() {
-                });
-        return updateShopsApiResponse;
+                HttpMethod.PUT, updateShopRequest, new ParameterizedTypeReference<>() {});
+        assertNotNull(updateShopsApiResponse);
+        assertSame(HttpStatus.CREATED, updateShopsApiResponse.getStatusCode());
     }
 
     @Test
     void deleteShopAndVerifiesStatus() {
-        ResponseEntity<ShopModel> shopModel = createShopRequest();
-        ResponseEntity<List<ShopModel>> createShopsApiResponse = testRestTemplate.exchange(createURLForGetRequest("/shops", host, testPort),
-                HttpMethod.POST, null, new ParameterizedTypeReference<>() {
-                });
-        ResponseEntity<ShopModel> shop = deleteShop(shopModel.getBody().getId());
-        assertNotNull(shop);
-        assertSame(HttpStatus.OK, shop.getStatusCode());
+        final String id = "548446d66f9c421d61bb825d";
+        ResponseEntity<ShopModel> deleteShopResponse = testRestTemplate.exchange(createURLForGetRequest("/shop/" + id, host, testPort),
+                HttpMethod.DELETE, null, new ParameterizedTypeReference<>() {});
+        assertNotNull(deleteShopResponse);
+        assertSame(HttpStatus.OK, deleteShopResponse.getStatusCode());
     }
 
-    private ResponseEntity<ShopModel> deleteShop(String id) {
-        ResponseEntity<ShopModel> shops = testRestTemplate.exchange(createURLForGetRequest("/shop/" + id, host, testPort),
-                HttpMethod.DELETE, null, new ParameterizedTypeReference<>() {
-                });
-        return shops;
-    }
-
-    public static String createURLForGetRequest(final String uri, final String host,
+    private String createURLForGetRequest(final String uri, final String host,
                                                 final String port) {
-        System.out.println(port);
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("http://")
                 .append(host)
                 .append(":")
                 .append(port)
                 .append(uri);
-        System.out.println(stringBuilder);
         return stringBuilder.toString();
     }
 
